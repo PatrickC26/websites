a6a73943e2fde161ed8eb7aabd2b41cef60082a6
Patrick Chen <patrick910826@gmail.com>
2022-06-25 22:37:00 +0800

    rfid auth file added

---
+++ root/RFID_auth/RFID_Auth.js
@@ -0,0 +1,21 @@
+let worker;
+
+function load(){
+//    firebaseGET("admin");
+//    console.log(readAuth("internet"));
+//    putTime();
+
+    
+    worker = new Worker("worker.js")
+    // pass data to worker thread
+    worker.postMessage(name)
+    // listen to any data passed from worker thread
+    worker.addEventListener("message", event => {
+        
+    })
+}
+
+let SUCCESS = 0, NO_ACCESS = 2,FAIL = 1, LOCKED = 3,TIMEOUT = 4;
+
+let isLogin = false;
+
