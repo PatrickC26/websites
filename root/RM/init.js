71955bf4862cdd30d3ef8ef84901475730bbfe6f
Patrick Chen <patrick910826@gmail.com>
2022-06-28 16:40:05 +0800

    rfid auth file added

--- root/RM/init.js
+++ root/RM/init.js
@@ -1,9 +1,11 @@
 
 let authPage = "https://patrickc26.github.io/websites/root/RFID_auth/RFID_auth.html?code=";
 
-let access = "aQ2Zh96618D";
+let access = "aQ2Zh96618D"; // should only edit HERE
 
 let auth = new URLSearchParams(window.location.search).get("auth");
+
+window.history.replaceState("","",location.href.substring(0, location.href.indexOf("?") ).toString());
 
 console.log(auth);
 
@@ -19,19 +21,17 @@ else if (auth == firebaseGET("Access/" + access + "/ac")){
     nowM -= 2;
     
     console.log(nowM);
+    firebasePUT("Access/" + access + "/ac", "");
     
-    if (nowM < parseInt(auth.substring(auth.indexOf("T")+1, auth.length))){
-        console.log("SUCCESS");
-        
-        const nextState = { additionalInformation: 'Updated the URL with JS' };
-        window.history.replaceState("","RM",location.href.substring(0, location.href.indexOf("?") ).toString());
-        
-        firebasePUT("Access/" + access + "/ac", "");
+    console.log(parseInt(auth.substring(auth.lastIndexOf("T")+1, auth.length)));
+    
+    if (nowM < parseInt(auth.substring(auth.lastIndexOf("T")+1, auth.length))){
+        console.log("Auth SUCCESS");
     }
     else {
         window.alert("Time out\nPlease Login again");
+        window.location.href = authPage + access;
     }
-    
 }
 
 else{
